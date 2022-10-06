7c1679e46c6971631c635170b2c5a65382064ded
Oscar Rovira <560186+krosk93@users.noreply.github.com>
2022-10-06 09:27:02 +0200

    Add scheming and other changes

--- ckanext/stomallorca_theme/plugin.py
+++ ckanext/stomallorca_theme/plugin.py
@@ -1,10 +1,22 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckan.lib.plugins import DefaultTranslation
+from ckan.common import config
+
+def default_locale():
+    '''Wrap the ckan default locale in a helper function to access
+    in templates.
+    Returns 'en' by default.
+    :rtype: string
+    '''
+    value = config.get('ckan.locale_default', 'en')
+    return value
 
 class StomallorcaThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.ITranslation)
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
+#    plugins.implements(plugins.IPackageController)
 
     # IConfigurer
 
@@ -12,4 +24,38 @@ class StomallorcaThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "stomallorca_theme")
+        config_['scheming.dataset_schemas'] = config_.get('scheming.dataset_schemas', "") + """
+ ckanext.stomallorca_theme:schemas/dataset.yaml
+ """
+
+    # ITemplateHelpers
+    def get_helpers(self):
+        """
+        Provide template helper functions
+        """
+        return {
+            'extrafields_default_locale': default_locale,
+        }
+
+    # IPackageController
+    def before_index(self, data_dict):
+        titles = json.loads(data_dict.get('title_translated', '{}'))
+        data_dict['title_ca'] = titles.get('ca', '')
+        data_dict['title_en'] = titles.get('en', '')
+        data_dict['title_string'] = titles.get('es', '')
+
+    def read(self, entity):
+        pass
+
+    def create(self, entity):
+        pass
+
+    def edit(self, entity):
+        pass
+
+    def delete(self, entity):
+        pass
+
+    def before_search(self, search_params):
+        pass
 
