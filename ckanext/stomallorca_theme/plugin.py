a7ed4dadd5042a98ffcad23c679978652ad000fa
Oscar Rovira <560186+krosk93@users.noreply.github.com>
2022-10-14 14:51:02 +0200

    TC OD15: Keep language

--- ckanext/stomallorca_theme/plugin.py
+++ ckanext/stomallorca_theme/plugin.py
@@ -2,6 +2,7 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckan.lib.plugins import DefaultTranslation
 from ckan.common import config
+import ckan.lib.helpers as h
 
 def default_locale():
     '''Wrap the ckan default locale in a helper function to access
@@ -12,11 +13,16 @@ def default_locale():
     value = config.get('ckan.locale_default', 'en')
     return value
 
+def stomallorca_portal_url():
+    portal_url = config.get('ckanext.stomallorca_theme.portal_url', 'https://stomallorca.com/')
+    if (h.lang() != 'es'):
+      portal_url += h.lang() + '/'
+    return portal_url
+
 class StomallorcaThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.ITranslation)
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers)
-#    plugins.implements(plugins.IPackageController)
 
     # IConfigurer
 
@@ -32,27 +38,6 @@ class StomallorcaThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
         """
         return {
             'extrafields_default_locale': default_locale,
+            'stomallorca_portal_url': stomallorca_portal_url,
         }
-
-    # IPackageController
-    def before_index(self, data_dict):
-        titles = json.loads(data_dict.get('title_translated', '{}'))
-        data_dict['title_ca'] = titles.get('ca', '')
-        data_dict['title_en'] = titles.get('en', '')
-        data_dict['title_string'] = titles.get('es', '')
-
-    def read(self, entity):
-        pass
-
-    def create(self, entity):
-        pass
-
-    def edit(self, entity):
-        pass
-
-    def delete(self, entity):
-        pass
-
-    def before_search(self, search_params):
-        pass
 
